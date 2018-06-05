b593757560f2d61bc6a5ee8a98dc6523e25ff5af
Dark.Hades <dark.hades.1102@gmail.com>
2018-06-05 16:34:47 +0900

    Add Billing source code

---
+++ include/Billing/stdafx.cpp
@@ -0,0 +1,12 @@
+// stdafx.cpp : 只包括标准包含文件的源文件
+// WX_Billing.pch 将作为预编译头
+// stdafx.obj 将包含预编译类型信息
+
+#include "stdafx.h"
+
+#ifdef __WINDOWS__
+#pragma comment(lib, "ws2_32.lib")
+#endif
+
+// TODO: 在 STDAFX.H 中
+// 引用任何所需的附加头文件，而不是在此文件中引用
